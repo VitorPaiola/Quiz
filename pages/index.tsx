8f11a2dc454cc180c4b857925752690c3732795b
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-08-28 18:48:58 -0300

    Domínio modificado

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import QuestaoModel from "../model/questao";
 import Questionario from "../components/Questionario";
 import { useRouter } from "next/router";
 
-const BASE_URL = "https://quiz-lilac-one.vercel.app/"
+const BASE_URL = "https://quiz-lilac-one.vercel.app/api"
 
 export default function Home() {
   const router = useRouter()
